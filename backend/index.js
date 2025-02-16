5cbdeb6ed0342f697007c34c4c4294760695ea73
Rounak-Ghosh <rounakghosh189@gmail.com>
2025-02-16 15:19:45 +0530

    architechture changed

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files from the "public" directory
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, '../frontend/public')));
 
 
 const connectToMongoDb = () => {
